34adeb2a409ebef180034547134b3f9ee1053915
MD Niyaz Hashmi <mdniyazhashmi@example.com>
2025-12-03 09:46:21 +0530

    updated github actions yaml file

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -12,7 +12,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
 import static com.constants.Browser.*;
 
 public class TestBase {
